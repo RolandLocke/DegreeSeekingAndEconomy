32a5b12e8d8a1c732d7119ebc09c721370410a20
RolandLocke <foogabob@gmail.com>
2024-02-12 13:40:13 +1300

    added a new py file for editing graphs

---
+++ Graphs.py
@@ -0,0 +1,13 @@
+"""
+Roland Locke
+2/12/24
+
+In this document, the data collected in DataCleaning.py is graphed effectively.
+
+making plots look nice:https://www.easytweaks.com/matplotlib-seaborn-text-label-points/
+"""
+
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from sklearn.preprocessing import MinMaxScaler
